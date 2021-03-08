330965be1922f0f3f52209f6293373ce7a6aafe9
AlexV-coder <61702518+AlexV-coder@users.noreply.github.com>
2021-03-08 17:58:35 +0200

    Enemies and bullets
    Enemies follow the player and attack it. The player can shoot bullets and destroy the enemies.

---
+++ GD Project/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    float delay = 3.0f; //This implies a delay of 2 seconds.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, delay);
+    }
+}
