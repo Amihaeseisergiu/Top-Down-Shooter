dd63f2ee8412f27bb8bf8a510a3a5c54c4b72a8c
AlexV-coder <61702518+AlexV-coder@users.noreply.github.com>
2021-03-08 17:48:35 +0200

    Add files via upload

---
+++ GD Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, 0.05f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "Bullet(Clone)")
+        {
+            Destroy(gameObject);
+        } else if (col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
